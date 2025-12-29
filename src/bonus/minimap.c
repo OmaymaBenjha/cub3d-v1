fa81f4e74be99aedff2418d85afd4048044d6711
t-rik <ziane.aymn@gmail.com>
2025-12-29 16:02:16 +0100

    minor

--- src/bonus/minimap.c
+++ src/bonus/minimap.c
@@ -9,7 +9,6 @@ static void	put_pixel(t_textures *img, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-
 static void    draw_mp(t_game *g)
 {
     int x;
@@ -29,6 +28,8 @@ static void    draw_mp(t_game *g)
 				put_pixel(&g->img_buffer, x, y, COLOR_VOID);
 			else if (g->map[my][mx] == '1')
 				put_pixel(&g->img_buffer, x, y, COLOR_WALL);
+			else if (g->map[my][mx] == 'D' || g->map[my][mx] == 'O')
+				put_pixel(&g->img_buffer, x, y, COLOR_DOOR);
 			else if (g->map[my][mx] == '0')
 				put_pixel(&g->img_buffer, x, y, COLOR_FLOOR);
 			else
@@ -43,8 +44,8 @@ static void	draw_p(t_game *g)
 	int	i;
 	int	j;
 
-	cx = MINI_W / 2;
-	cy = MINI_H / 2;
+	cx = (MINI_W / 2) + 5;
+	cy = (MINI_H / 2) + 5;
 	i = -3;
 	while (i <= 3)
 	{
@@ -67,8 +68,8 @@ static void	draw_pd(t_game *g)
 	i = 0;
 	while (i < 15)
 	{
-		x = MINI_W / 2 + g->p.dir_x * i;
-		y = MINI_H / 2 + g->p.dir_y * i;
+		x = (MINI_W / 2) + 5 + g->p.dir_x * i;
+		y = (MINI_H / 2) + 5 + g->p.dir_y * i;
 		put_pixel(&g->img_buffer, (int)x, (int)y, COLOR_DIR);
 		i++;
 	}
